9e37451e4342626ac796f2e89b04627be159ac3d
GY <guoyang134340@163.com>
2019-06-04 01:17:47 +0800

    add test

--- test.scm
+++ test.scm
@@ -1,2 +1,10 @@
-(define size 2)
-size
+(define (square x) (* x x))
+; (square 21)
+(define (sum-of-squares x y)
+  (+ (square x) (square y))
+)
+; (sum-of-squares  3 4)
+(define (f a)
+  (sum-of-squares (+ a 1) (* a 2))
+)
+; (f 5)
